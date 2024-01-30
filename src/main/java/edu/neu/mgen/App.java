6a6c87584011b82281b6338028c3fb6a986ee606
jiabeiliu <145812963+jiabeiliu@users.noreply.github.com>
2024-01-30 11:23:20 -0800

    first commit

---
+++ src/main/java/edu/neu/mgen/App.java
@@ -0,0 +1,21 @@
+package edu.neu.mgen;
+
+
+// This is a single-line comment. 
+public class App {
+    public static void main(String[] args) {
+        System.out.println("It is my first Java program");
+
+        /**
+ * This is a multi-line comment.
+ * @author Jiabei Liu
+ *  2024-01-30
+ */
+
+    }
+}
+
+
+
+
+  
